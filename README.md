b1030ba71311a5361d897ab75d365d75dd3e6ad4
Bill Love <wjlove@gmail.com>
2022-06-14 10:47:22 -0400

    Initial repo load after rename
    Renamed the repo and start over

---
+++ README.md
@@ -0,0 +1,36 @@
+# simple-zeroconf
+
+This is fork of python-zeroconf by Jakub Stasiak (https://github.com/jstasiak/python-zeroconf). It has been modified to simply advertise a single service via DNS-SD via a balena block.
+
+Compatible with:
+
+* Bonjour
+* Avahi
+
+## Usage
+
+Here is an example of using this block in your `docker-compose.yml`.  
+
+```
+version: '2.1'
+services:
+
+  simple-zeroconf:
+    image: bh.cr/william_love/simple-zeroconf-<arch>
+    restart: "always"
+    network_mode: host
+    environment:
+      ZEROCONF_SERVICE_PORT: 8080
+      ZEROCONF_SERVICE_TYPE: "_mytestservice"
+      ZEROCONF_SERVICE_DESCRIPTION: "My Test Service"
+    labels:
+      io.balena.features.supervisor-api: '1'
+
+```
+Currently the supported `<arch>` are `arm7hf`, `aarch64` and `amd64`.
+
+The environment variable ZEROCONF_SERVICE_TYPE defines the name of the service to advertise (the leading "_" is required), while ZEROCONF_SERVICE_PORT and ZEROCONF_SERVICE_DESCRIPTION respectively define the port the service can be found on and a short description of the service.
+
+You can also set these variables via the balena dashboard, see [this](https://www.balena.io/docs/learn/manage/variables/) for more information about setting variables in the balenaCloud.
+
+Note:  This block uses the balena supervisor API to discover the host name of the hostOS as visiable to the local network to be able to create the full service name.  If you change the hostname using the balena supervisor API the block will notice the change and update the service advertisement.  For more inforation about working with the supervisor API refer to this [documentation](https://www.balena.io/docs/reference/supervisor/supervisor-api/#http-api-reference).
---
+++ registration.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+
+""" Example of announcing a service (in this case, a balenAIR senor) """
+
+import argparse
+import logging
+import socket
+import os
+import atexit
+from time import sleep
+import json
+import urllib.request
+from urllib.error import HTTPError, URLError
+
+from zeroconf import IPVersion, ServiceInfo, Zeroconf
+
+try:
+    serviceDescriptionTxt = os.getenv('ZEROCONF_SERVICE_DESCRIPTION', 'No Descrition')
+except Exception as e:
+    serviceDescriptionTxt = "No Description"
+    print("Invalid value for ZEROCONF_SERVICE_DESCRIPTION, using default")
+try:
+    serviceType = os.getenv('ZEROCONF_SERVICE_TYPE', '_balenair')
+    serviceType = serviceType + "._tcp.local."
+except Exception as e:
+    serviceType = "_balenair._tcp.local."
+    print("Invalid value for ZEROCONF_SERVICE_TYPE, using default")
+try:
+    servicePort = int(os.getenv('ZEROCONF_SERVICE_PORT', '1880'))
+except Exception as e:
+    servicePort = 1880
+    print("Invalid value for ZEROCONF_SERVICE_PORT, using default")
+#
+# print("From ENV settings, Description:", serviceDescriptionTxt, "Type:", serviceType, "Port:", servicePort)
+
+# Get supervisor info, Actually if either one of these fail we should just bail.....
+try:
+    supervisorAddress = os.getenv('BALENA_SUPERVISOR_ADDRESS', '127.0.0.1')
+except Exception as e:
+    supervisorAddress = "Fail"
+    print("Invalid value for BALENA_SUPERVISOR_ADDRESS, using default")
+try:
+    supervisorApiKey = os.getenv('BALENA_SUPERVISOR_API_KEY', '127.0.0.1')
+except Exception as e:
+    supervisorApiKey = "Fail"
+    print("Invalid value for BALENA_SUPERVISOR_ADDRESS_API_KEY, using default")
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug', action='store_true')
+    version_group = parser.add_mutually_exclusive_group()
+    version_group.add_argument('--v6', action='store_true')
+    version_group.add_argument('--v6-only', action='store_true')
+    args = parser.parse_args()
+
+    if args.debug:
+        logging.getLogger('zeroconf').setLevel(logging.DEBUG)
+    if args.v6:
+        ip_version = IPVersion.All
+    elif args.v6_only:
+        ip_version = IPVersion.V6Only
+    else:
+        ip_version = IPVersion.V4Only
+
+
+    # Stuff we need for the service
+    serviceDescription = {'Device Type': serviceDescriptionTxt}
+    hostName = socket.gethostname()
+    # print("Hostname", hostName)
+
+    # Build a URL for accessing the balena supoervisor API
+    supervisorURL = supervisorAddress + "/v1/device?apikey=" + supervisorApiKey
+
+    # Get what the balena supervisor believes is the local ip address of the device.  Depends on the container running in network_mode: host
+    try:
+        deviceRawData = urllib.request.urlopen(supervisorURL, timeout=10).read().decode()
+    except HTTPError as error:
+        print('HTTP Error: Data not retrieved because %s\nURL: %s', error, supervisorURL)
+    except URLError as error:
+        if isinstance(error.reason, socket.timeout):
+            print('Timeout Error: Data not retrieved because %s\nURL: %s',  error, supervisorURL)
+        else:
+            print('URL Error: some other error....')
+
+    deviceInfo = json.loads(deviceRawData)
+    serviceAddress = deviceInfo['ip_address']
+    serviceName = hostName + "." + serviceType
+    print("Advertising this service:", serviceName, " Service address: ", serviceAddress, "Service Type:", serviceType)
+
+    info = ServiceInfo(
+        serviceType,
+        name=serviceName,
+        addresses=[socket.inet_aton(serviceAddress)],
+        port=servicePort,
+        properties=serviceDescription,
+        server=hostName + ".local.",
+    )
+
+    def exitGracefully(cleanUpInfo):
+        print("Unregistering service...")
+        zeroconf.unregister_service(cleanUpInfo)
+        zeroconf.close()
+
+    zeroconf = Zeroconf(ip_version=ip_version)
+    print("Registration of", serviceType, "service complete")
+    zeroconf.register_service(info)
+
+    # We really just need to sleep forever, this code left in place for testing.
+    try:
+        while True:
+            sleep(0.1)
+    except KeyboardInterrupt:
+        pass
+    except SystemExit:
+        pass
+    finally:
+        # print("Unregistering...")
+        # zeroconf.unregister_service(info)
+        # zeroconf.close()
+        exitGracefully(info)
